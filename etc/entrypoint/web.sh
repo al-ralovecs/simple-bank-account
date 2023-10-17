45b8e5b634749c3bd807397cb0621d8301864854
Aleksandrs Ralovecs <aleksandrs.ralovecs@gmail.com>
2023-10-17 16:36:09 +0300

    Update: some bug fixes.

--- etc/entrypoint/web.sh
+++ etc/entrypoint/web.sh
@@ -29,6 +29,10 @@ printf '%s\n' "[$(date +"%T")] Setting up log directory" >&1
 [ ! -d "${APP_DIR}/var/log" ] && mkdir -p "${APP_DIR}/var/log"
 chmod -R 0777 "${APP_DIR}/var/log"
 
+printf '%s\n' "[$(date +"%T")] Setting up cache directory" >&1
+[ ! -d "${APP_DIR}/var/cache/dev" ] && mkdir -p "${APP_DIR}/var/cache/dev"
+chmod -R 0777 "${APP_DIR}/var/cache/dev"
+
 printf '%s\n' "[$(date +"%T")] Building project" >&1
 su-exec ${APP_USER_NAME} ./bin/build.sh
 
